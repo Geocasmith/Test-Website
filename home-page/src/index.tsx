f897b8ea7e9003b00603a0b9bf57c95ea0307920
George Carr-Smith <gca73@uclive.ac.nz>
2022-09-20 15:19:15 +1200

    Added boat image #feature

--- home-page/src/index.tsx
+++ home-page/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {DesktopHomepage} from "./ui-components";
+import '@aws-amplify/ui-react/styles.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+<App/>
+      {/*<DesktopHomepage/>*/}
   </React.StrictMode>
 );
 
